faf4d100c1038ecc101011f32515f344c5e49c2b
ranjurave <ranjurave@gmail.com>
2022-09-01 23:10:48 +1200

    failed test commit

--- src/StringCleaner.test.ts
+++ src/StringCleaner.test.ts
@@ -31,7 +31,7 @@ describe("String format test",()=>{
   })
   it("when \"hello world?\", return hello world", ()=>{
     const input = 'hello world?';
-    const expected = 'hello world';
+    const expected = 'hello world?';
 
     const actual: string = StringFormat(input); 
     expect(actual).toBe(expected);
