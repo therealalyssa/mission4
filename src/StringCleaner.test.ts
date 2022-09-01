0a3ae93928f4da33735ac40f0dd79e738155841c
ranjurave <ranjurave@gmail.com>
2022-09-01 21:34:19 +1200

    test added

---
+++ src/StringCleaner.test.ts
@@ -0,0 +1,39 @@
+import StringFormat from "./StringCleaner"
+
+describe("String format test",()=>{
+  it("when \"hello%world\", return hello world", ()=>{
+    const input = 'hello%world';
+    const expected = 'hello world';
+
+    const actual: string = StringFormat(input); 
+    expect(actual).toBe(expected);
+  })
+  it("when \"!hello world\", return hello world", ()=>{
+    const input = '!hello world';
+    const expected = 'hello world';
+
+    const actual: string = StringFormat(input); 
+    expect(actual).toBe(expected);
+  })
+  it("when \";hello?world!\", return hello world", ()=>{
+    const input = ';hello?world!';
+    const expected = 'hello world';
+
+    const actual: string = StringFormat(input); 
+    expect(actual).toBe(expected);
+  })
+  it("when \"hello;world\", return hello world", ()=>{
+    const input = 'hello;world';
+    const expected = 'hello world';
+
+    const actual: string = StringFormat(input); 
+    expect(actual).toBe(expected);
+  })
+  it("when \"hello world?\", return hello world", ()=>{
+    const input = 'hello world?';
+    const expected = 'hello world?';
+
+    const actual: string = StringFormat(input); 
+    expect(actual).toBe(expected);
+  })
+})
